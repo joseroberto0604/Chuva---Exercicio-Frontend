c84f3a9f34a1bc56aaffea6ddabab9f3d0b44e59
jose roberto <79425225+joseroberto0604@users.noreply.github.com>
2022-06-10 23:37:55 -0300

    Add files via upload

--- chuva.js
+++ chuva.js
@@ -1,17 +1,3 @@
-function showMore() {
-    let summaryText = document.querySelector("#section-summary-text")
-    summaryText.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae turpis auctor, mollis felis ut, commodo turpis. Phasellus felis mauris, egestas eget cursus et, iaculis quis lacus. Fusce auctor eros sed magna ultricies gravida. Etiam aliquam dictum nisl, vel aliquet enim accumsan sit amet. Donec finibus nisi tellus, ut viverra lorem vestibulum ut. Phasellus condimentum orci id leo condimentum lobortis et non lorem. Sed id nisl metus. Quisque sollicitudin ligula in sapien scelerisque, ac gravida eros vestibulum.
-    <br>
-    <br>
-    Fusce vitae luctus dui. Donec id euismod mauris, in volutpat urna. Proin dapibus consequat feugiat. Proin dictum justo arcu, quis vestibulum augue lacinia quis. Sed dignissim dui nulla, ut faucibus mauris sodales id. Aliquam erat volutpat. Maecenas dolor enim, tincidunt id elit non, suscipit interdum turpis. Etiam finibus urna libero, eget interdum eros volutpat ullamcorper. Pellentesque et pretium lorem. Aenean interdum quis diam ac porttitor. Cras nec ipsum pulvinar, pharetra libero non, ornare enim. Etiam in laoreet odio.
-    <br>
-    <br>
-    Nam eget tristique elit, at fermentum tellus. Mauris scelerisque ligula id eleifend feugiat. Donec eleifend vehicula sem nec dapibus. Integer scelerisque neque dui, in lacinia erat molestie eu. Phasellus maximus dui eget lacus porta tempor. Ut ex nibh, dignissim quis purus semper, efficitur facilisis turpis. Praesent blandit elementum ultricies. Aliquam sit amet enim sit amet nulla pulvinar lobortis consectetur non odio. Phasellus at lacus hendrerit, vulputate nisi sit amet, viverra mauris. Etiam eu scelerisque orci. Quisque sagittis, mi vitae pharetra iaculis, orci elit eleifend massa, eu posuere mauris odio id odio. Morbi eu libero luctus, consectetur lorem vel, interdum sapien. Aenean in porta arcu. Maecenas eu maximus massa.\n\n
-    <br>
-    <br>
-    Praesent velit dolor, dignissim sed quam ac, efficitur porta justo. Pellentesque porta pharetra felis ut hendrerit. Nulla facilisi. Aliquam erat volutpat. Nunc sit amet faucibus quam. Maecenas dapibus luctus dolor at viverra. Duis nec fringilla libero. Duis risus nibh, viverra ac orci nec, iaculis dictum sem. Aliquam at malesuada arcu. Aliquam erat volutpat. Donec varius ipsum purus, ut vehicula purus placerat vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. `
-}
-
 let enviarTopico = document.querySelector(".section-criarTopico-form")
 let topicoEnviado = document.querySelector(".section-discussion-topicoCriado")
 
@@ -37,5 +23,28 @@ function showAnswers() {
     }
     else {
         document.querySelector(".section-discussion-answers-mainContainer").style.display = 'none'
+    }
+}
+function toggleText(){
+
+    var texto = 
+        document.getElementById("texto");
+  
+    var pontos = 
+        document.getElementById("pontos");
+
+    var showMoreText =
+        document.getElementById("moreText");
+
+    if (pontos.style.display === "none") {
+        texto.style.display = "block"
+        showMoreText.style.display = "none";
+        pontos.style.display = "inline";
+    }
+
+    else {
+        texto.style.display = "none"
+        showMoreText.style.display = "block";
+        pontos.style.display = "none";
     }
 }
